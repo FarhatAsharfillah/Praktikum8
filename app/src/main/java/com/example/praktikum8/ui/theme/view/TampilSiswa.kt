3b4edfd7fcf7703e99d2f406d73cbbc791588969
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-11-20 15:22:18 +0700

    menambahkan scaffold isiruang di tampilsiswa

--- app/src/main/java/com/example/praktikum8/ui/theme/view/TampilSiswa.kt
+++ app/src/main/java/com/example/praktikum8/ui/theme/view/TampilSiswa.kt
@@ -40,3 +40,39 @@ fun TampilSiswa(
         Pair(stringResource(id = R.string.jenis_kelamin), statusUiSiswa.gender),
         Pair(stringResource(id = R.string.alamat), statusUiSiswa.alamat),
     )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id= R.string.tampil),
+                        color = Color.White
+                    )},
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource(id=R.color.teal_700))
+            )
+        }){isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(),
+                            fontSize = 16.sp)
+                        Text(text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackButtonClicked) {
+                    Text(text = stringResource(id=R.string.back))
+                }
+            }
+        }
+    }
+}
